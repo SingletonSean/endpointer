555e75b8e9d699c1c8ec32de00317550b49366f0
SingletonSean <singletonsean4@gmail.com>
2021-01-01 20:56:28 -0500

    Add AutoMapper

---
+++ Endpointer.Authentication.API/Mappers/DomainResponseProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Endpointer.Authentication.API.Models;
+using Endpointer.Core.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Endpointer.Authentication.API.Mappers
+{
+    public class DomainResponseProfile : Profile
+    {
+        public DomainResponseProfile()
+        {
+            CreateMap<AuthenticatedUser, AuthenticatedUserResponse>();
+        }
+    }
+}
---
+++ Endpointer.Authentication.API/Models/AuthenticatedUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Endpointer.Authentication.API.Models
+{
+    public class AuthenticatedUser
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime AccessTokenExpirationTime { get; set; }
+    }
+}
