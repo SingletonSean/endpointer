dc7e65527b5270ce84a6c78ea9b0d55f566edb06
SingletonSean <singletonsean4@gmail.com>
2021-01-08 18:35:27 -0500

    Add API builder tests

---
+++ Endpointer.Accounts.API.Tests/Models/EndpointerAccountsOptionsBuilderTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using Endpointer.Accounts.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Endpointer.Accounts.API.Tests.Models
+{
+    [TestFixture()]
+    public class EndpointerAccountsOptionsBuilderTests
+    {
+        private EndpointerAccountsOptionsBuilder _builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = new EndpointerAccountsOptionsBuilder();
+        }
+
+        [Test()]
+        public void Build_WithDatabase_ReturnsOptionsWithUseDatabaseTrue()
+        {
+            EndpointerAccountsOptions options = _builder.WithDatabase().Build();
+
+            Assert.IsTrue(options.UseDatabase);
+        }
+
+        [Test()]
+        public void Build_WithDatabase_ReturnsOptionsWithDatabaseServices()
+        {
+            EndpointerAccountsOptions options = _builder.WithDatabase().Build();
+
+            Assert.IsNotNull(options.AddDbContext);
+            Assert.IsNotNull(options.AddDbAccountRepository);
+        }
+
+        [Test()]
+        public void Build_WithoutConfiguration_ReturnsOptionsWithUseDatabaseFalse()
+        {
+            EndpointerAccountsOptions options = _builder.Build();
+
+            Assert.IsFalse(options.UseDatabase);
+        }
+
+        [Test()]
+        public void Build_WithoutConfiguration_ReturnsOptionsWithoutDatabaseServices()
+        {
+            EndpointerAccountsOptions options = _builder.Build();
+
+            Assert.IsNull(options.AddDbContext);
+            Assert.IsNull(options.AddDbAccountRepository);
+        }
+    }
+}
---
+++ Endpointer.Authentication.API.Tests/Models/EndpointerAuthenticationOptionsBuilderTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Endpointer.Authentication.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Endpointer.Authentication.API.Tests.Models
+{
+    [TestFixture()]
+    public class EndpointerAuthenticationOptionsBuilderTests
+    {
+        private EndpointerAuthenticationOptionsBuilder _builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = new EndpointerAuthenticationOptionsBuilder();
+        }
+
+        [Test()]
+        public void Build_WithDatabase_ReturnsOptionsWithUseDatabaseTrue()
+        {
+            EndpointerAuthenticationOptions options = _builder.WithDatabase().Build();
+
+            Assert.IsTrue(options.UseDatabase);
+        }
+
+        [Test()]
+        public void Build_WithDatabase_ReturnsOptionsWithDatabaseServices()
+        {
+            EndpointerAuthenticationOptions options = _builder.WithDatabase().Build();
+
+            Assert.IsNotNull(options.AddDbContext);
+            Assert.IsNotNull(options.AddDbRefreshTokenRepository);
+            Assert.IsNotNull(options.AddDbUserRepository);
+        }
+
+        [Test()]
+        public void Build_WithoutConfiguration_ReturnsOptionsWithUseDatabaseFalse()
+        {
+            EndpointerAuthenticationOptions options = _builder.Build();
+
+            Assert.IsFalse(options.UseDatabase);
+        }
+
+        [Test()]
+        public void Build_WithoutConfiguration_ReturnsOptionsWithoutDatabaseServices()
+        {
+            EndpointerAuthenticationOptions options = _builder.Build();
+
+            Assert.IsNull(options.AddDbContext);
+            Assert.IsNull(options.AddDbRefreshTokenRepository);
+            Assert.IsNull(options.AddDbUserRepository);
+        }
+    }
+}
