29908b0c6e4799267cd7f030b7b210d4e34deda8
SingletonSean <singletonsean4@gmail.com>
2021-02-09 17:07:09 -0500

    Add verify email request model

---
+++ Authentication/Endpointer.Authentication.Core/Models/Requests/VerifyEmailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Endpointer.Authentication.Core.Models.Requests
+{
+    /// <summary>
+    /// Model for a verify email request.
+    /// </summary>
+    public class VerifyEmailRequest
+    {
+        public string VerifyToken { get; set; }
+    }
+}
